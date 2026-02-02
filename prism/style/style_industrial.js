2b336f03881da3f9ab9c548ce17a43471b01a8a7
xiayian505-ship-it <xiayian505@gmail.com>
2026-02-02 13:54:20 +0800

    Add files via upload

---
+++ prism/style/style_industrial.js
@@ -0,0 +1,6 @@
+window.STYLE_DATA = {
+  name: "industrial",
+  colors: [
+    "#2E2E2E","#3C3F41","#4F5356","#616568"
+  ]
+};
---
+++ prism/style/style_nordic.js
@@ -0,0 +1,6 @@
+window.STYLE_DATA = {
+  name: "nordic",
+  colors: [
+    "#ECEFF1","#CFD8DC","#B0BEC5","#90A4AE"
+  ]
+};
